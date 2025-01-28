06f939e6e592f4eaeeb466198da0b392e1667df9
shubha shyam <155746431+shubha-shyam@users.noreply.github.com>
2025-01-28 23:43:55 +0100

    Created parameters.sh

---
+++ deployment/parameters.sh
@@ -0,0 +1,25 @@
+# Copyright 2022 Amazon Web Services, Inc
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+IDC_LOGIN_URL=https://amboss-aws.awsapps.com/start
+REGION=eu-west-1
+TEAM_ACCOUNT=302567802819
+ORG_MASTER_PROFILE=AWSAdministratorAccess-072646712121
+TEAM_ACCOUNT_PROFILE=AWSAdministratorAccess-302567802819
+TEAM_ADMIN_GROUP="AWS Access"
+TEAM_AUDITOR_GROUP="TEAM-Auditor"
+TAGS="Team=platform"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:eu-west-1:302567802819:eventdatastore/05ec73a3-0978-4c26-9baa-6d4bcfa07881
+SECRET_NAME=TEAM-Elevated-Access
+UI_DOMAIN=team.amboss.in
